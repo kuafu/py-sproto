7cc5a7a021c4188b52da60147a05b13de4368371
coder <coder>
2016-11-29 21:23:07 +0800

    scripts

---
+++ bin/Scripts/ue_site.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python2
+import sys
+sys.path.append('/Scripts')
+print("sys.path:")
+for line in sys.path: print(line)
+print("-------------------------------")
+
+from sproto import Sproto
+
+with open("./test.sp") as f:
+    chunk = f.read()
+
+sp = Sproto(chunk)
+spbin = sp.encode("AddressBook", 
+                  {"person": [
+                      {"name": "Alice", "id": 10000},
+                      {"name": "Bob", "id":200, "email":"hotmail"},
+                  ],
+                   "names" :["Alice", "Bob", "vv"],
+               })
+
+print(sp.decode("AddressBook", sp.unpack(sp.pack(spbin))))
+
+req, resp,tag = sp.protocal("call")
+
+print("foo request:", sp.decode(req, sp.encode(req, {"name":"whq", "email":"give@money.com"})))
+print("foo response:", sp.decode(resp, sp.encode(resp, {"ok": True, "reason":"GoodJob"})))
